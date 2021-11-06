b8256dc380af13ef42ab4ca52faa1617858819a7
Freditansari <Freditansari@users.noreply.github.com>
2021-11-06 17:49:40 +0700

    change development port according to azure

--- app.py
+++ app.py
@@ -134,4 +134,4 @@ def banana():  # put application's code here
     return "Analytics chart"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5555)
+    app.run(debug=True, host='0.0.0.0', port=5000)
